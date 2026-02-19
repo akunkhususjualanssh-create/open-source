7fa236e99ada36e00e5642845e36d50ce0d691a0
akunkhususjualanssh-create <akunkhususjualanssh@gmail.com>
2026-02-20 06:14:36 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/Rehanzcraker/Sc-hanzvip/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/akunkhususjualanssh-create/open-source/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
